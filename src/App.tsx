e8112e1c8211b3a6c8152791137e99292e4be674
Keiko Higuchi <keiko_higuchi@claris.com>
2024-09-01 18:15:06 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'; //React 16以前では必要
+import Counter from './Counter';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My Counter App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
+// React 18以降での標準的なレンダリング方法。
+// 以前のバージョンでは、以下のような従来のレンダリング方法を使用する。
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
